0ce3e00e60f620b80c5b9606e7c05756e79ce042
salvator <ssalvator@gmail.com>
2012-02-14 07:02:19 -0500

    bootstrap 2.0

--- admintools_bootstrap/models.py
+++ admintools_bootstrap/models.py
@@ -23,5 +23,17 @@ from admin_tools.menu import items
 # addming icon argument to base MenuItem class
 items.MenuItem.icon = None
 
-
 import admintools_bootstrap.settings
+
+# patching django's FieldSet
+from django.contrib.admin import helpers
+from django import forms
+
+class Fieldset(helpers.Fieldset):
+    def _media(self):
+        if 'collapse' in self.classes:
+            return forms.Media(js=['admintools_bootstrap/bootstrap/js/bootstrap-collapse.js'])
+        return forms.Media()
+    media = property(_media)
+
+helpers.Fieldset = Fieldset
--- admintools_bootstrap/static/admintools_bootstrap/js/init-ui.js
+++ admintools_bootstrap/static/admintools_bootstrap/js/init-ui.js
@@ -1,6 +1,6 @@
 $(function(){
 	$('select').chosen();
 	$('.vDateField').datepicker({dateFormat: 'yy-mm-dd'});
-	$("a[rel=popover].top-icon").twipsy({offset: 20});
+//	$("a[rel=popover].top-icon").twipsy({offset: 20});
 });
 
