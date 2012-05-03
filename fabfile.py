419c6d68bf8a3c3192fa2fc952b24c6b7445d350
Travis Swicegood <development@domain51.com>
2012-05-03 15:57:01 -0500

    Add in compiled version of Bootstrap 2.0.3

--- fabfile.py
+++ fabfile.py
@@ -3,15 +3,19 @@ from os.path import join, dirname
 
 
 BOOTSTRAP_PATH = join(dirname(__file__), "vendor", "bootstrap")
+COMPILED_BOOTSTRAP_PATH = join(BOOTSTRAP_PATH, "docs", "assets")
 DESTINATION_PATH = join(dirname(__file__), "admintools_bootstrap", "static",
         "admintools_bootstrap", "bootstrap")
 
 
 @task
 def update_bootstrap():
-    local("cp %s/js/*.js %s/js/" % (BOOTSTRAP_PATH, DESTINATION_PATH))
+    local("cd vendor/bootstrap && make")
+    local("cp %s/js/bootstrap.min.js %s/js/" % (
+            COMPILED_BOOTSTRAP_PATH, DESTINATION_PATH))
     local("cp %s/img/*.* %s/img" % (BOOTSTRAP_PATH, DESTINATION_PATH))
-    local("cp %s/less/*.* %s/less" % (BOOTSTRAP_PATH, DESTINATION_PATH))
+    local("cp %s/css/bootstrap*.css %s/css" % (
+            COMPILED_BOOTSTRAP_PATH, DESTINATION_PATH))
     # for path in ["img", "js", "less", "LICENSE"]:
     #     full_path = join(BOOTSTRAP_PATH, path)
     #     local("cp -R %s %s" % (full_path, DESTINATION_PATH))
