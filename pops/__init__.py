abfc7495eb62ad6426b8ee794fbc08643553e8f6
Travis Swicegood <development@domain51.com>
2012-05-14 16:23:40 -0500

    Update the setup.py to use flunky-style setup.py

--- pops/__init__.py
+++ pops/__init__.py
@@ -1,1 +1,13 @@
-__version__ = (0, 0, 2, "alpha", 0)
+major = "0"
+minor = "1"
+third = "0alpha"
+extra = "0"
+
+# You should not need to edit anything beyond this point.
+__version_parts__ = (major, minor, third)
+
+if not third.isdigit() or extra is not 0:
+    __version_parts__ += (extra, )
+__version__ = ".".join(__version_parts__)
+
+version = "%s version %s" % (__name__, __version__)
